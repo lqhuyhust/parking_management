3eef8493e3874c537370feba6d583880b310e8b9
huylq8498 <huylq8498@co-well.vn>
2021-11-25 11:30:48 +0700

    test api

--- car/views.py
+++ car/views.py
@@ -2,30 +2,36 @@ from rest_framework import status
 from .models import Car
 from .serialzers import CarSerializer, CarSingleSerializer
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.views import APIView
 
 # Create your views here.
 class CarList(APIView):
+    permission_classes = (AllowAny, )
+    # def get(self, request, *args, **kwargs):
+    #     if self.request.user.is_superuser:
+    #         cars = Car.objects.all()
+    #     elif self.request.user.role.role == 'Guest':
+    #         cars = Car.objects.get(user=self.request.user.id)
+    #     else:
+    #         return Response('Not found', status=status.HTTP_404_NOT_FOUND)
+        
+    #     serializer = CarSerializer(cars, many=True)
+    #     return Response(serializer.data)
     def get(self, request, *args, **kwargs):
-        if self.request.user.is_superuser:
-            cars = Car.objects.all()
-        elif self.request.user.role.role == 'Guest':
-            cars = Car.objects.get(user=self.request.user.id)
-        else:
-            return Response('Not found', status=status.HTTP_404_NOT_FOUND)
+        cars = Car.objects.all()
         
         serializer = CarSerializer(cars, many=True)
         return Response(serializer.data)
     
-    def post(self, request, *args, **kwargs):
-        if not (self.request.user.role.role == 'Guest' or self.request.user.is_superuser):
-            return Response('Unauthorized', status=status.HTTP_403_FORBIDDEN)
-        serializer = CarSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request, *args, **kwargs):
+    #     if not (self.request.user.role.role == 'Guest' or self.request.user.is_superuser):
+    #         return Response('Unauthorized', status=status.HTTP_403_FORBIDDEN)
+    #     serializer = CarSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CarDetail(APIView):
     """

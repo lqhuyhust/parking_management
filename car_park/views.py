eb6065bf103e730f3a29ac4976247cc9483ab08e
huylq8498 <huylq8498@co-well.vn>
2021-11-30 11:11:46 +0700

    calculate distanc

--- car_park/views.py
+++ car_park/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from .models import CarPark, ParkingSlot, Port
 from .serializers import CarParkSerializer, CarParkSingleSerializer, ParkingSlotSerializer, ParkingSlotSingleSerializer, PortSerializer, PortSingleSerializer
-from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView
+from geopy.distance import geodesic
 
 # Create your views here.
 class CarParkList(generics.ListCreateAPIView):
-    permission_classes = (AllowAny, )
+    permission_classes = (IsAdminUser, )
     queryset = CarPark.objects.all()
     serializer_class = CarParkSerializer
 
@@ -33,3 +36,18 @@ class PortDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAdminUser, )
     queryset = Port.objects.all()
     serializer_class = PortSingleSerializer
+
+class SearchCarPark(APIView):
+    def get(self, request, *args, **kwargs):
+        longitude = kwargs.get('long')
+        latitude = kwargs.get('lat')
+        target = ("{:.6f}".format(float(longitude)), "{:.6f}".format(float(latitude)))
+    
+        car_parks = CarPark.objects.all()
+        serializer = CarParkSerializer(car_parks, many=True)
+        for car_park in serializer.data:
+            print(("{:.6f}".format(float(car_park['longitude'])), "{:.6f}".format(float(car_park['latitude']))))
+            coordinate = ("{:.6f}".format(float(car_park['longitude'])) + ',' + "{:.6f}".format(float(car_park['latitude'])))
+            car_park['distance'] = geodesic(target,coordinate).km
+        print(serializer.data)
+        Response(serializer.data)     

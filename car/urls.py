951bb98db9630f56f6d9a5a5ea59ff4b12dc04e2
huylq8498 <huylq8498@co-well.vn>
2021-12-14 09:50:52 +0700

    remove api add new car

--- car/urls.py
+++ car/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from car import views
 
 urlpatterns = [
-    path('', views.CarList.as_view()),
-    path('<str:username>', views.CarDetail.as_view()),
+    path('<str:username>', views.CarDetail.as_view())
 ]

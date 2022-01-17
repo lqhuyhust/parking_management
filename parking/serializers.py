e4e0c36cad1f03064cf57aaac1fe80efa208ae7c
huylq8498 <huylq8498@co-well.vn>
2022-01-17 14:23:46 +0700

    update get parking record

--- parking/serializers.py
+++ parking/serializers.py
@@ -6,4 +6,4 @@ class ParkingSerializer(serializers.ModelSerializer):
     parking_slot = serializers.StringRelatedField()
     class Meta:
         model = Parking
-        fields = ('id', 'car_park', 'parking_slot', 'time_start', 'time_end', 'fee', 'status' )
+        fields = ('id', 'car_park', 'parking_slot', 'time_start', 'time_end', 'fee', 'status', 'qr_code' )
